c3a9b202e94337db2ef674fa5a5d943dea2a9ecb
Rishiraj <rishirajnatj@gmail.com>
2026-05-24 02:25:58 +0530

    feat: add index file to export all controllers for better modularity

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export { AuthController } from './auth.controller';
+export { RunController } from './run.controller';
+export { TerritoryController } from './territory.controller';
+export { LeaderboardController } from './leaderboard.controller';
+export { SocialController } from './social.controller';
+export { ProfileController } from './profile.controller';

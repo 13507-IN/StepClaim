d6eec933c5aa75ff37af6c1cc51ce7bed2e61fe9
Rishiraj <rishirajnatj@gmail.com>
2026-05-24 02:24:09 +0530

    feat: create configuration index file to export environment and application constants

---
+++ backend/src/config/index.ts
@@ -0,0 +1,13 @@
+export { env, type Env } from './env.js';
+export { prisma } from './database.js';
+export { redis, disconnectRedis } from './redis.js';
+export { cloudinary } from './cloudinary.js';
+export {
+  H3_RESOLUTION,
+  XP_RATES,
+  SPEED_LIMITS,
+  ANTI_CHEAT,
+  LEVEL_THRESHOLDS,
+  STREAK,
+  PAGINATION,
+} from './constants.js';
